b3c2c9b21e79d52e12f350494843fcede5bc185d
Thammarith <8011543+thammarith@users.noreply.github.com>
2022-07-03 23:18:18 +0700

    Add console

--- src/libs/apollo.ts
+++ src/libs/apollo.ts
@@ -1,9 +1,11 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { baseUrl } from "@constants/urls";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { baseUrl } from '@constants/urls';
+
+console.log(`Making requests to ${baseUrl}`);
 
 const client = new ApolloClient({
-    uri: `${baseUrl}/graphql`,
-    cache: new InMemoryCache(),
+	uri: `${baseUrl}/graphql`,
+	cache: new InMemoryCache(),
 });
 
 export default client;
